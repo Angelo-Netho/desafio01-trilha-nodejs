5fb8e0780a21abff94adddf7603530fa937392b5
Angelo-Netho <angelonetho@gmail.com>
2022-07-30 00:09:04 -0300

    Criar todo

--- src/index.js
+++ src/index.js
@@ -50,12 +50,26 @@ app.post('/users', (request, response) => {
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  const user = request.user;
+  const { user } = request;
   return response.json(user.todos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline} = request.body;
+  const { user } = request;
+
+  const todo = 
+  {
+    id: uuidv4,
+    title,
+    done: false,
+    deadline: new Date(deadline),
+    created_at: new Date()
+  }
+
+  user.todos.push(todo)
+
+  return response.status(201).json(todo);
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
